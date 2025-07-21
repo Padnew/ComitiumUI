075de66c70d1a140bf3ab87e1d3dd0c0240732c1
Paddy <Paddygnewton@hotmail.co.uk>
2025-07-21 20:57:04 +0100

    Export and story for header

---
+++ src/components/Header/Header.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Header } from "./index";
+
+const meta: Meta<typeof Header> = {
+  title: "Components/Header",
+  component: Header,
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof Header>;
+
+export const DefaultHeader: Story = {
+  render: () => (
+    <Header heading="Big heading" size="3rem" subHeading="Smaller heading" />
+  ),
+};
