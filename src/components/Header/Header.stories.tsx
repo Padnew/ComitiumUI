86f3918c712868bb53e2aa26a7cd1b391dd09c1e
Paddy <Paddygnewton@hotmail.co.uk>
2025-07-21 21:00:06 +0100

    Add default and large stories

--- src/components/Header/Header.stories.tsx
+++ src/components/Header/Header.stories.tsx
@@ -10,8 +10,13 @@ const meta: Meta<typeof Header> = {
 export default meta;
 type Story = StoryObj<typeof Header>;
 
-export const DefaultHeader: Story = {
+export const LargeHeader: Story = {
   render: () => (
     <Header heading="Big heading" size="3rem" subHeading="Smaller heading" />
+  ),
+};
+export const DefaultHeader: Story = {
+  render: () => (
+    <Header heading="Default heading" subHeading="Default Subheading" />
   ),
 };
