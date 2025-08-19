25df6e46897d171b9de451ad708fc9bf40d418fe
Paddy <Paddygnewton@hotmail.co.uk>
2025-08-19 18:27:13 +0100

    Added container

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { resolveShadow } from "../../utils/ShadowResolver";
+import { resolveRadius } from "../../utils/RadiusResolver";
+import { resolvePadding } from "../../utils/SizeResolver";
+
+type ContainerProps = {
+  shadow?: "none" | "sm" | "md" | "lg";
+  children?: React.ReactNode;
+  style?: React.CSSProperties;
+  curved?: "none" | "sm" | "md" | "lg";
+  backgroundColor?: string;
+  padding?: "sm" | "md" | "lg";
+};
+
+export const Container: React.FC<ContainerProps> = ({
+  shadow = "none",
+  children,
+  style,
+  curved = "none",
+  backgroundColor = "inherit",
+  padding = "md",
+}) => {
+  const baseStyle: React.CSSProperties = {
+    boxShadow: resolveShadow(shadow),
+    borderRadius: resolveRadius(curved),
+    padding: resolvePadding(padding),
+    backgroundColor,
+    ...style,
+  };
+
+  return <div style={baseStyle}>{children}</div>;
+};
