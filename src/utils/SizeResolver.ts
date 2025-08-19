be30640a637736a199b36b2bc34426c9ffa196e3
Paddy <Paddygnewton@hotmail.co.uk>
2025-08-19 18:29:19 +0100

    Better padding values

--- src/utils/SizeResolver.ts
+++ src/utils/SizeResolver.ts
@@ -15,9 +15,9 @@ export const resolveSize = (size?: string): string => {
   };
 
   export const resolvePadding = (size?: string): string => {
-    if (size === "sm") return "0.75rem";
-    if (size === "md") return "0.875rem";
-    if (size === "lg") return "1rem";
-    if (size === "xl") return "1.125rem";
-    return "0.875rem";
+    if (size === "sm") return "1rem";
+    if (size === "md") return "1.25rem";
+    if (size === "lg") return "2rem";
+    if (size === "xl") return "2.25rem";
+    return "1rem"
   };
