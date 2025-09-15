6c53b3dc2e992e5f96aa63a2919fc344b0dbb9ef
Paddy <Paddygnewton@hotmail.co.uk>
2025-09-15 20:22:15 +0100

    fix: broken textinput stories

--- src/components/TextInput/TextInput.stories.tsx
+++ src/components/TextInput/TextInput.stories.tsx
@@ -11,14 +11,14 @@ export default meta;
 type Story = StoryObj<typeof TextInput>;
 
 export const DefaultTextInput: Story = {
-  render: () => <TextInput />,
+  render: () => <TextInput label="Default Input" />,
 };
 export const LabelTextInput: Story = {
-  render: () => <TextInput label="XL Username:" size="xl" />,
+  render: () => <TextInput label="XL Username:" fs="xl" />,
 };
 
 export const SensitiveTextInput: Story = {
   render: () => (
-    <TextInput variant="sensitive" size="lg" label="Large Password:" />
+    <TextInput variant="sensitive" fs="lg" label="Large Password:" />
   ),
 };
